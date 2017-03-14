4c0ff61adb347ade0e5fcfffbb410a23dd34e157
liuyakun <happy_lucky52@163.com>
2017-03-14 22:20:41 +0800

    max_subsquence

--- chapter4/maxsubsquence_update/main.cpp
+++ chapter4/maxsubsquence_update/main.cpp
@@ -3,29 +3,31 @@
 using std::cout;
 using std::endl;
 
-int max_subsquence_A (int *arr, int sz, int &l, int &r);
+int max_subsquence_A (int *arr, int sz, int &l, int &r);//转化为增量，右端索引可以求出，但是左端不太好求，本程序所求左端不正确
+int max_subsquence_B (int *arr, int sz, int &l, int &r);//暴力求解
 
 void print_array (int *arr, int sz);
 void test_max_subsquence_A ();
+void test_max_subsquence_B ();
 
 int main (int argc, char *argv[])
 {
 	test_max_subsquence_A ();
+	test_max_subsquence_B ();
 
 
 	return 0;
 
 }
 
-int max_subsquence_A (int *arr, int sz, int &l, int &r)//暴力求解
+int max_subsquence_A (int *arr, int sz, int &l, int &r)
 {
 	int max = arr[0];
-	int sum = 0;
 	for (int i = 0; i < sz; ++i)
 	{
-		sum = arr[i];
+		int sum = arr[i];
 
-		for (int j = i; j < sz; ++j)
+		for (int j = i + 1; j < sz; ++j)
 		{
 			l = j;
 			sum += arr[j];
@@ -37,7 +39,7 @@ int max_subsquence_A (int *arr, int sz, int &l, int &r)//暴力求解
 		}
 	}
 
-	return sum;
+	return max;
 }
 
 void test_max_subsquence_A ()
@@ -55,6 +57,22 @@ void test_max_subsquence_A ()
 	cout << "The max subsquence is : " << max << " from " << left << " to " << right << endl;
 }
 
+void test_max_subsquence_B ()
+{
+	int iArr[] = {100, 113, 110, 85, 105, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97};
+	int sz = sizeof (iArr) / sizeof (int);
+
+	cout << "The sequence is : " << endl;
+	print_array (iArr, sz);
+
+	int left = 0;
+	int right = 0;
+
+	int max = max_subsquence_B (iArr, sz, left, right);
+	cout << "The max subsquence is : " << max << " from " << left << " to " << right << endl;
+
+}
+
 void print_array (int *arr, int sz)
 {
 	for (int i = 0; i < sz; ++i)
@@ -62,3 +80,21 @@ void print_array (int *arr, int sz)
 	cout << endl;
 }
 
+int max_subsquence_B (int *arr, int sz, int &l, int &r)//暴力求解
+{
+	int max = 0;//这里初始化为0
+	for (int i = 0; i < sz; ++i)
+	{
+		for (int j = i + 1; j < sz; ++j)
+		{
+			if (arr[j] - arr[i] > max)
+			{
+				max = arr[j] - arr[i];
+				l = i;
+				r = j;	
+			}
+		}
+	}
+
+	return max;
+}
